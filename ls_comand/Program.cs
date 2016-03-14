e4f0b15568a7d03c0201c9079e1174d839503161
Miguel Jose Balderas Díaz <Miguel Jose Balderas Daz>
2016-03-13 23:54:00 -0600

    iniciando

---
+++ ls_comand/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ls_comand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "cmd.exe";
+            psi.Arguments = "/c " + "dir";
+            psi.RedirectStandardOutput = true;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = false;
+
+            Process proc = new Process();
+            proc.StartInfo = psi;
+            proc.Start();
+            Console.WriteLine(proc.StandardOutput.ReadToEnd());
+        }
+    }
+}
